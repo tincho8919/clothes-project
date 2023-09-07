78b947f8a98d50de5ebe818bff2187549aa19612
tincho8919 <tincho8919@gmail.com>
2023-09-07 05:07:03 -0300

    otro4

--- js/inicio.js
+++ js/inicio.js
@@ -7,3 +7,46 @@ toggleButton.addEventListener("click", () => {
     menu.classList.toggle("active"); // Toggle (agregar/quitar) la clase "active" al menú
 });
 
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-btn');
+const nextButton = document.querySelector('.next-btn');
+
+function changeSlide(direction) {
+    currentSlide += direction;
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    } else if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+
+    const translateValue = -currentSlide * 100;
+    slider.style.transform = `translateX(${translateValue}%)`;
+}
+
+prevButton.addEventListener('click', () => {
+    changeSlide(-1);
+});
+
+nextButton.addEventListener('click', () => {
+    changeSlide(1);
+});
+
+//Auto-avance opcional
+setInterval(() => {
+    changeSlide(1);
+}, 3000);
+
+function buyProduct(productName) {
+    const message = document.getElementById(`message${productName.charAt(productName.length - 1)}`);
+    message.textContent = `Has comprado ${productName}.`;
+}
+
+// Limpiar los mensajes cuando se recarga la página
+window.addEventListener('load', () => {
+    const messages = document.querySelectorAll('.message');
+    messages.forEach(message => {
+        message.textContent = '';
+    });
+});
